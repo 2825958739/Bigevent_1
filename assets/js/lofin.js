19b7aca55a59cefbbd6a9ba011b88df822c3ff79
chenliang <2825958739@qq.com>
2022-03-14 16:23:17 +0800

    初步完善登录注册功能

---
+++ assets/js/lofin.js
@@ -0,0 +1,12 @@
+$(function () {
+    //点击
+    $("#link_reg").on("click", function () {
+        $(".login-box").hide()
+        $(".reg-box").show()
+    })
+
+    $("#link_login").on("click", function () {
+        $(".login-box").show()
+        $(".reg-box").hide()
+    })
+})
